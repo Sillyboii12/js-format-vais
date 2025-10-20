5d9f1de5d20eb74fecdd29142a2d9c4b67e37362
unknown <bruno@example.com>
2025-10-20 09:21:09 +0300

    fixed a block def problem

--- array-and-for-loop.js
+++ array-and-for-loop.js
@@ -1,5 +1,6 @@
 let fruit = ["Zemene", "Ķirsis", "Melene", "Dzērvene"];
 
-for (let i = 0; i < fruit.length; i++) (
-    console.log(fruit[i])
-)
+for (let i = 0; i < fruit.length; i++) {
+    console.log(fruit[i]);
+}
+
